b90d30b55222875b50c08302cd142829be11d3ba
Yuri <wall.mart.boss@proton.me>
2024-06-11 16:12:42 +0300

    work - step 2

---
+++ src/js/tech_skills.js
@@ -0,0 +1,4 @@
+const marquee = document.querySelector(".marquee");
+const marquee_tilt = document.querySelector(".marquee_tilt");
+marquee.classList.add("dark-mode");
+marquee_tilt.classList.add("dark-mode");
