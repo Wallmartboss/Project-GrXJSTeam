87c9b0c25c1e553acfa13bddfafccd3c74af6441
daria drozd <daria.drozd1990@gmail.com>
2024-06-13 22:10:32 +0300

    dark-mode

---
+++ src/js/about_me.js
@@ -0,0 +1,11 @@
+// const refs = {
+//     toggler:document.querySelector('.toggler'),
+// }
+// refs.toggler.addEventListener('click', changeHeaderColor)
+// function changeHeaderColor() {
+//     const headers = document.querySelectorAll('.aboutH2, .titel');
+//     headers.forEach(header => {
+//         header.classList.toggle('dark-mode');
+//     });
+// }
+
