3d4c84fbfc85fba89739abe2f2612a19f219e3cd
Yuri <wall.mart.boss@proton.me>
2024-06-14 22:09:48 +0300

    Fix dark

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 localStorage.removeItem('theme');
 document.addEventListener('DOMContentLoaded', () => {
-  // const toggleButton = document.getElementById('theme-toggle');
-  const toggleButton = document.querySelectorAll('.btn-toggle-checkbox');    /* tech_skills */
+  const toggleButton = document.querySelectorAll('.btn-toggle-checkbox');
   const body = document.body;
   const marquee = document.querySelector(".marquee");     /* tech_skills */
   const marquee_tilt = document.querySelector(".marquee_tilt");   /* tech_skills */
