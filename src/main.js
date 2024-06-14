da69f01349babd52ee380e1c6692fc9febf818de
Yuri <wall.mart.boss@proton.me>
2024-06-14 03:01:04 +0300

    fix header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   // const toggleButton = document.getElementById('theme-toggle');
-  const toggleButton = document.querySelector('.btn-toggle')    /* tech_skills */
+  const toggleButton = document.querySelectorAll('.btn-toggle');    /* tech_skills */
+  console.log(toggleButton);
   const body = document.body;
   const marquee = document.querySelector(".marquee");     /* tech_skills */
   const marquee_tilt = document.querySelector(".marquee_tilt");   /* tech_skills */
@@ -9,22 +10,22 @@ document.addEventListener('DOMContentLoaded', () => {
   if (savedTheme) {
     body.classList.add(savedTheme);
   }
+  toggleButton.forEach((tog) =>
+    tog.addEventListener('click', () => {
+      body.classList.toggle('dark-mode');
 
-  toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-
-    if (body.classList.contains('dark-mode')) {
-      localStorage.setItem('theme', 'dark-mode');
-      marquee.classList.add("dark-mode");    /* tech_skills */
-      marquee_tilt.classList.add("dark-mode");    /* tech_skills */
-      const bw_dot = document.querySelectorAll(".bw_dot"); /* tech_skills */
-      bw_dot.forEach((dot) => dot.classList.add("filter-white")); /* tech_skills */
-    } else {
-      localStorage.removeItem('theme');
-      marquee.classList.remove("dark-mode");  /* tech_skills */
-      marquee_tilt.classList.remove("dark-mode"); /* tech_skills */
-      const bw_dot = document.querySelectorAll(".bw_dot");    /* tech_skills */
-      bw_dot.forEach((dot) => dot.classList.remove("filter-white"));  /* tech_skills */
-    }
-  });
+      if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode');
+        marquee.classList.add("dark-mode");    /* tech_skills */
+        marquee_tilt.classList.add("dark-mode");    /* tech_skills */
+        const bw_dot = document.querySelectorAll(".bw_dot"); /* tech_skills */
+        bw_dot.forEach((dot) => dot.classList.add("filter-white")); /* tech_skills */
+      } else {
+        localStorage.removeItem('theme');
+        marquee.classList.remove("dark-mode");  /* tech_skills */
+        marquee_tilt.classList.remove("dark-mode"); /* tech_skills */
+        const bw_dot = document.querySelectorAll(".bw_dot");    /* tech_skills */
+        bw_dot.forEach((dot) => dot.classList.remove("filter-white"));  /* tech_skills */
+      }
+    }));
 });
