73a0803a2b6fca907f58bf2fb7d699805311d90e
OlesyaAlexx <152548843+OlesyaAlexx@users.noreply.github.com>
2024-06-15 21:55:16 +0300

    footer js change

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,60 +1,92 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
+  // Отримуємо посилання на необхідні елементи
   const cookieSettingsLink = document.getElementById('cookie-settings-link');
   const cookieSettingsModal = document.getElementById('cookie-settings-modal');
   const closeCookieSettings = document.getElementById('close-cookie-settings');
   const saveCookieSettings = document.getElementById('save-cookie-settings');
   const cookieSettingsForm = document.getElementById('cookie-settings-form');
 
-  // Перевірка наявності елементів
-  if (
-    cookieSettingsLink &&
-    cookieSettingsModal &&
-    closeCookieSettings &&
-    saveCookieSettings &&
-    cookieSettingsForm
-  ) {
-    // Відкриття модального вікна
-    cookieSettingsLink.addEventListener('click', function (event) {
-      event.preventDefault();
-      cookieSettingsModal.classList.remove('modal-hide');
-      cookieSettingsModal.classList.add('modal-show');
-    });
+  // Функція для налаштування роботи з  cookies
+  function setupCookieSettings() {
+    // Перевіряємо чи всі необхідних елементів є в наявності
+    if (
+      cookieSettingsLink &&
+      cookieSettingsModal &&
+      closeCookieSettings &&
+      saveCookieSettings &&
+      cookieSettingsForm
+    ) {
+      // Додаємо слухача події для відкриття модального вікна
+      cookieSettingsLink.addEventListener('click', event => {
+        event.preventDefault();
+        cookieSettingsModal.classList.remove('modal-hide');
+        cookieSettingsModal.classList.add('modal-show');
+      });
 
-    // Закриття модального вікна
-    const closeModal = () => {
-      cookieSettingsModal.classList.remove('modal-show');
-      cookieSettingsModal.classList.add('modal-hide');
+      // Додаємо слухача події для закриття модального вікна
+      closeCookieSettings.addEventListener('click', closeModal);
+
+      // Додаємо слухача події для закриття модального вікна при кліку поза ним
+      cookieSettingsModal.addEventListener('click', event => {
+        if (event.target === event.currentTarget) {
+          closeModal();
+        }
+      });
+
+      // Додаємо слухача події для збереження налаштувань cookies
+      saveCookieSettings.addEventListener('click', saveSettings);
+
+      // Застосувуєм збережені налаштування
+      applySavedSettings();
+    }
+  }
+
+  // Функція для закриття модального вікна
+  function closeModal() {
+    cookieSettingsModal.classList.remove('modal-show');
+    cookieSettingsModal.classList.add('modal-hide');
+  }
+
+  // Функція для збереження налаштувань cookies
+  function saveSettings() {
+    const { analytics, security } = cookieSettingsForm.elements;
+    const settings = {
+      analytics: analytics.checked,
+      security: security.checked,
     };
 
-    closeCookieSettings.addEventListener('click', closeModal);
-
-    // Закриття модального вікна при натисканні за його межами
-    cookieSettingsModal.addEventListener('click', function (event) {
-      if (event.target === event.currentTarget) {
-        closeModal();
-      }
-    });
-
-    // Збереження налаштувань cookies
-    saveCookieSettings.addEventListener('click', function () {
-      const settings = {
-        analytics: cookieSettingsForm.elements['analytics'].checked,
-        advertising: cookieSettingsForm.elements['advertising'].checked,
-      };
+    // Зберігаємо налаштування в локальному сховищі
+    try {
       localStorage.setItem('cookieSettings', JSON.stringify(settings));
-      closeModal();
-    });
+    } catch (e) {
+      console.error('Could not save settings to localStorage', e);
+    }
 
-    // Застосування налаштувань що були збережені під час завантаження сторінки
-    const applySavedSettings = () => {
+    // Закриваємо модальне вікно після збереження налаштувань
+    closeModal();
+  }
+
+  // Функція для застосування збережених налаштувань
+  function applySavedSettings() {
+    // Намагаємось отримати налаштування що були збережені в локальному сховищі
+    try {
       const savedSettings = localStorage.getItem('cookieSettings');
       if (savedSettings) {
+        // Якщо збережені налаштування існують, вони застосовуються до форми
         const settings = JSON.parse(savedSettings);
-        cookieSettingsForm.elements['analytics'].checked = settings.analytics;
-        cookieSettingsForm.elements['advertising'].checked =
-          settings.advertising;
+        const { analytics, security } = cookieSettingsForm.elements;
+
+        analytics.checked = settings.analytics || false;
+        security.checked = settings.security || false;
+      } else {
+        console.error('No settings found in localStorage');
       }
-    };
-    applySavedSettings();
+    } catch (e) {
+      // Обробляємо помилку при отриманні налаштувань з локального сховища
+      console.error('Could not retrieve settings from localStorage', e);
+    }
   }
+
+  // Викликаємо функцію для налагодження роботи з налаштуваннями cookies
+  setupCookieSettings();
 });
