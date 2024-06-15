80fd47aa4285318ec024542526ce9221e78b2506
OlhaMasl <maslakok@gmail.com>
2024-06-15 08:42:16 +0300

    reviews js

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,27 +1,51 @@
 // core version + navigation, pagination modules:
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation'; 
 
-const swiper = new Swiper('.swiper', {
-  // configure Swiper to use modules
-  modules: [Navigation, Pagination],
-  ...
-});
 
+const reviewsList = document.querySelector(".swiper-wrapper")
 
 function getReviews() {
     const url = "https://portfolio-js.b.goit.study/api/reviews"   
     
      return fetch(url).then(res => {
          if (!res.ok) {
-             throw new Error(console.log(res));  
+             throw new Error(res.statusText);  
          }
          return res.json();
      });
 };
 
 const results = getReviews();
+console.log(results);
+results.then((data) => {
+    console.log(data);
+    const markup = reviewsMarkup(data);
+    reviewsList.innerHTML = markup;
+}).catch((error) => {
+    console.log(error);
+})
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  modules: [Navigation],
+  direction: 'horizontal',
+    loop: true,
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+function reviewsMarkup(arrReviews) {
+    return arrReviews.map((oneReview) => {
+       return `<li class="swiper-slide"><p class="review-text">${oneReview.review}</p>
+            <div class="review-author">
+                <img src="${oneReview.avatar_url}" class="review-author-photo" alt="Photo ${oneReview.author}"
+                    width="40" height="40">
+                <p class="review-author-name">${oneReview.author}</p>
+            </div></li>`
+    }).join("");
+}
