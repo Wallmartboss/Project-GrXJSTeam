3be12605323c384b45e1bc1637151e4bcb921965
daria drozd <daria.drozd1990@gmail.com>
2024-06-15 11:59:09 +0300

    background-image update

--- src/js/about_me.js
+++ src/js/about_me.js
@@ -1,15 +1,2 @@
-const toggleButton = document.querySelector('.toggler'); /* tech_skills */
+const toggleButton = document.querySelector('.toggler');
 
-//     toggleButton.addEventListener('click', changeHeaderColor)
-// function changeHeaderColor() {
-//     console.log("ppppp");
-// //     const headers = document.querySelectorAll('.aboutH2, .titel');
-// //     headers.forEach(header => {
-// //         header.classList.toggle('dark-mode');
-// //     });
-// }
-
-// toggleButton.addEventListener('click', () => {
-//   aboutH2.classList.toggle('dark-mode')
-//   if(body.classList.contains('dark-mode'))
-// });
